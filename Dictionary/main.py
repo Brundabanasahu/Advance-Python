77020ecdad55ca0b6a6889202e62620c686852e0
Brundabana Sahu <sbrundabana12@gmail.com>
2026-02-06 09:28:52 +0530

    Dictionary

--- Dictionary/main.py
+++ Dictionary/main.py
@@ -4,3 +4,19 @@ d1={
     "city": "Bangalore"
     }
 print(d1)
+dict={}
+print(type(dict))
+print(len(d1))
+print(d1["name"])
+
+d2={
+    "rcb": "Virat Kohli",
+    "mi": "Rohit Sharma",
+    "csk": "MS Dhoni"
+}
+print(d2)
+d2["kkr"]="Gautam Gambhir"
+print(d2)
+print(d2["csk"])
+print(len(d2))
+
