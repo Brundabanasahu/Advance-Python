2b87db0d7060baffcbdaf24c7fcb983dcdfef464
Brundabana Sahu <sbrundabana12@gmail.com>
2026-02-13 09:03:20 +0530

    Function Arguments

--- function/argument.py
+++ function/argument.py
@@ -10,4 +10,10 @@ student1("Brundabana", "23CSE584", 20, "J")
 
 def student2(name,rollno,age,section="J"):
     print(f"My Name is : {name} and Roll No is : {rollno} and my Age is : {age} and my Section is : {section}")
-student2(name="Brundabana", rollno="23CSE584", age=20, section="I")   
+student2(name="Brundabana", rollno="23CSE584", age=20, section="I") 
+
+def fruit(name):
+    for i in name:
+        print(i)  
+x=["apple","banana","orange"]        
+fruit(x)
