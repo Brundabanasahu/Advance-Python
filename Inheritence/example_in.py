e6db0969a285e0a3ae66e1897ce4e01ac86a3c82
thenameisadityya <as67jsr@gmail.com>
2026-02-25 10:33:14 +0530

    inheritance

---
+++ Inheritence/example_in.py
@@ -0,0 +1,11 @@
+class fatherdog:
+    def barkk(self):
+        print("dog is howling!!")
+
+class puppy(fatherdog):
+    def weeping(self):
+        print("puppy is weepinggg!")
+        
+p = puppy()
+p.barkk()
+p.weeping()
