1c2efb862b45b0762ac9de8fcfda38f20b0b2d00
Brundabana Sahu <sbrundabana12@gmail.com>
2026-02-25 10:27:03 +0530

    OOPS concept

--- Inheritence/single.py
+++ Inheritence/single.py
@@ -1,10 +1,23 @@
-class parent:
-    def show():
-        print("This is parent class")
-class child(parent):
-    def display():
-        print("This is child class")
+# class parent:
+#     def show():
+#         print("This is parent class")
+# class child(parent):
+#     def display():
+#         print("This is child class")
 
-c=child
-c.display()
-c.show()
+# c=child
+# c.display()
+# c.show()
+
+
+
+class Animal:
+    def eat(self):
+        print("Animal is eating")
+
+class dog(Animal):
+    def bark():
+        print("Dog is barking")   
+d=dog
+d.bark()
+d.eat()            
