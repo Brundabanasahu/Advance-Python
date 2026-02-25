fb8911268ccca4ddacec72c5ac7b6a1b9d103e65
Brundabana Sahu <sbrundabana12@gmail.com>
2026-02-25 11:22:16 +0530

    Inheritence

--- Inheritence/Hierarchical.py
+++ Inheritence/Hierarchical.py
@@ -1,31 +1,36 @@
-class Bank:
-    def details(self):
-        print("details of the customer")
+# class Bank:
+#     def details(self):
+#         print("details of the customer")
 
-class SavingAccount(Bank):
-    def sav(self):
-        print("This is saving account")
-
-
-class CurrentAccount(Bank):
-    def cur(self):
-        print("This is current account")
-
-class SalaryAccount(Bank):  
-    def sal(self):
-        print("This is salary account")
-
-sal=SalaryAccount()
-sal.details()
-sal.sal()
-print("------------------------------")
-
-c=CurrentAccount()
-c.details()
-c.cur()
-print("------------------------------")
+# class SavingAccount(Bank):
+#     def sav(self):
+#         print("This is saving account")
 
 
-s=SavingAccount()
-s.details()  
-s.sav()                
+# class CurrentAccount(Bank):
+#     def cur(self):
+#         print("This is current account")
+
+# class SalaryAccount(Bank):  
+#     def sal(self):
+#         print("This is salary account")
+
+# sal=SalaryAccount()
+# sal.details()
+# sal.sal()
+# print("------------------------------")
+
+# c=CurrentAccount()
+# c.details()
+# c.cur()
+# print("------------------------------")
+
+
+# s=SavingAccount()
+# s.details()  
+# s.sav()           
+     
+
+
+
+
